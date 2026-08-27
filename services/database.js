1a087ccfd52d5f6b3fc177349183f54ef8d4f8e7
Trigger <triggerww@outlook.com>
2026-08-27 14:54:38 -0300

    fasklnhasjf

--- services/database.js
+++ services/database.js
@@ -1,7 +1,18 @@
 const { initializeApp, cert } = require('firebase-admin/app');
 const { getFirestore } = require('firebase-admin/firestore');
+const fs = require('fs');
 
-const serviceAccount = require('../serviceAccountKey.json');
+let serviceAccount;
+
+if (process.env.FIREBASE_PROJECT_ID) {
+    serviceAccount = {
+        projectId: process.env.FIREBASE_PROJECT_ID,
+        clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+        privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n')
+    };
+} else {
+    serviceAccount = require('../serviceAccountKey.json');
+}
 
 initializeApp({
     credential: cert(serviceAccount),
