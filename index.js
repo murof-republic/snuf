c886cc83900a62c16ece02012c8c4bb251e285a1
Trigger <triggerww@outlook.com>
2026-08-26 21:45:09 -0300

    adição do deploy de comandos +

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 // Require the necessary discord.js classes
-const { Client, Events, GatewayIntentBits } = require('discord.js');
+const fs = require('node:fs');
+const path = require('node:path');
+const { Client, Collection, Events, GatewayIntentBits, MessageFlags } = require('discord.js');
 const { token } = require('./config.json');
 
 // Create a new client instance
@@ -10,6 +12,53 @@ const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 // It makes some properties non-nullable.
 client.once(Events.ClientReady, (readyClient) => {
 	console.log(`Pronto! Conectado como ${readyClient.user.tag}`);
+});
+
+client.commands = new Collection(); 
+
+const foldersPath = path.join(__dirname, 'commands');
+const commandFolders = fs.readdirSync(foldersPath);
+
+for (const folder of commandFolders) {
+	const commandsPath = path.join(foldersPath, folder);
+	const commandFiles = fs.readdirSync(commandsPath).filter((file) => file.endsWith('.js'));
+	for (const file of commandFiles) {
+		const filePath = path.join(commandsPath, file);
+		const command = require(filePath);
+		// Set a new item in the Collection with the key as the command name and the value as the exported module
+		if ('data' in command && 'execute' in command) {
+			client.commands.set(command.data.name, command);
+		} else {
+			console.log(`[WARNING] O comando em ${filePath} está sem a propriedade obrigatória "data" ou "execute".`);
+		}
+	}
+}
+
+client.on(Events.InteractionCreate, async (interaction) => {
+	if (!interaction.isChatInputCommand()) return;
+	const command = interaction.client.commands.get(interaction.commandName);
+
+	if (!command) {
+		console.error(`Não foi encontrado nenhum comando correspondente a ${interaction.commandName}.`);
+		return;
+	}
+
+	try {
+		await command.execute(interaction);
+	} catch (error) {
+		console.error(error);
+		if (interaction.replied || interaction.deferred) {
+			await interaction.followUp({
+				content: 'Houve um erro ao executar este comando!',
+				flags: MessageFlags.Ephemeral,
+			});
+		} else {
+			await interaction.reply({
+				content: 'Houve um erro ao executar este comando!',
+				flags: MessageFlags.Ephemeral,
+			});
+		}
+	}
 });
 
 // Log in to Discord with your client's token
