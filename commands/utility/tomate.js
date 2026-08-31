84eb1f8a44c8c3710d989104be80bc01916a1fee
Trigger <triggerww@outlook.com>
2026-08-31 16:49:18 -0300

    ajustes

--- commands/utility/tomate.js
+++ commands/utility/tomate.js
@@ -1,15 +1,28 @@
-// comando slash 'tomate' - requer discord.js v14
-// Esse comando foi gerado pelo proprio snuf
-// https://discord.com/channels/1526283360144588900/1526283361084244031/1542857908726010016
 const { SlashCommandBuilder } = require('discord.js');
 
 module.exports = {
-  data: new SlashCommandBuilder()
-    .setName('tomate')
-    .setDescription('Atira um tomate a alguém.')
-    .addUserOption(opt => opt.setName('alvo').setDescription('Quem vai levar o tomate.').setRequired(true)),
-  async execute(interaction) {
-    const alvo = interaction.options.getUser('alvo');
-    await interaction.reply(`${interaction.user} atirou um 🍅 em ${alvo}!`);
-  },
+	data: new SlashCommandBuilder()
+		.setName('tomate')
+		.setDescription('Atira um tomate a alguém.')
+		.addUserOption(opt =>
+			opt
+				.setName('alvo')
+				.setDescription('Quem vai levar o tomate.')
+				.setRequired(true)
+		),
+
+	async execute(interaction) {
+		const alvo = interaction.options.getUser('alvo');
+
+		if (alvo.id === interaction.client.user.id) {
+			return interaction.reply(
+				`${alvo} jogou um 🍅 de volta em ${interaction.user}!\n` +
+				`Tá maluco? Não mexe comigo !`
+			);
+		}
+
+		await interaction.reply(
+			`${interaction.user} atirou um 🍅 em ${alvo}!`
+		);
+	},
 };
